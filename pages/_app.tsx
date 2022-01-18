fb81c89bf8107369808ac9573c7099084a63d7e2
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2022-01-18 14:19:15 +0530

    feat: add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,27 @@
 import { NextSeo } from "next-seo";
 import { AppProps } from "next/app";
 import "../styles/globals.css";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+    strategy="lazyOnload"
+    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+  />
+
+  <Script id="google-analytics" strategy="lazyOnload">
+    {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+                `}
+  </Script>
+
       <NextSeo
         title="Connect with Avneesh"
         titleTemplate="Connect with Avneesh"
