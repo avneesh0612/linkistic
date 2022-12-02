13198d9268b2d3e1654b195377fc3e87f439d161
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2022-12-02 13:49:23 +0530

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,33 @@
-import Head from "next/head";
 import Image from "next/image";
 import Link from "../components/Link";
 import links from "../public/links.json";
+import { default as NextLink } from "next/link";
 
-export default function Home() {
+const Home = () => {
   return (
     <div className="flex flex-col w-screen min-h-screen overflow-x-hidden bg-background justify-center items-center">
       <div className="mt-5">
         <Image
           width={250}
           height={250}
-          className="mx-auto border-[1px] rounded-full border-accent"
+          className="mx-auto"
           src="/icon-512x512.png"
           alt="Avneesh Agarwal"
         />
         <h2 className="mb-3 -mt-5 text-3xl font-bold text-center text-accent md:text-4xl">
           Avneesh Agarwal
         </h2>
+        <p className="text-center text-white">
+          Developer Advocate at{" "}
+          <NextLink
+            href="https://www.thirdweb.com/"
+            className="text-accent underline"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            thirdweb
+          </NextLink>
+        </p>
       </div>
       <div className="w-full max-w-lg p-6">
         {links.map((link, i) => (
@@ -31,4 +42,6 @@ export default function Home() {
       </div>
     </div>
   );
-}
+};
+
+export default Home;
